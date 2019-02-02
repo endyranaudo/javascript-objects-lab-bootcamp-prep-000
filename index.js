4fb5dc7d5be127257533c6afd301af6706537ecf
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-02 16:25:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-let recipes = {};
+// let recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value})
-}
+// function updateObjectWithKeyAndValue(object, key, value) {
+//   return Object.assign({}, object, {[key]: value})
+// }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
-}
+// function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+//   object[key] = value
+//   return object
+// }
 
-function deleteFromObjectByKey(object, key){
-  var newRecipe = Object.assign({}, object)
-  delete newRecipe.key
-  return newRecipe
-}
+// function deleteFromObjectByKey(object, key){
+//   var newRecipe = Object.assign({}, object)
+//   delete newRecipe.key
+//   return newRecipe
+// }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
-  return object
-}
+// function destructivelyDeleteFromObjectByKey(object, key) {
+//   delete object.key
+//   return object
+// }
