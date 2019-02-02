a1305b1803c9dae75501a3522ac3f122eb2acc57
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-02 16:26:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
-// let recipes = {};
+let recipes = {};
 
-// function updateObjectWithKeyAndValue(object, key, value) {
-//   return Object.assign({}, object, {[key]: value})
-// }
-
-// function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-//   object[key] = value
-//   return object
-// }
-
-// function deleteFromObjectByKey(object, key){
-//   var newRecipe = Object.assign({}, object)
-//   delete newRecipe.key
-//   return newRecipe
-// }
-
-// function destructivelyDeleteFromObjectByKey(object, key) {
-//   delete object.key
-//   return object
-// }
-
-recipes = {}
-
- function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, { [key]: value })
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
 }
 
- function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
 }
 
- function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
-  delete newObj.key;
-  return newObj;
+function deleteFromObjectByKey(object, key){
+  var newRecipe = Object.assign({}, object)
+  delete newRecipe.key
+  return newRecipe
 }
 
- function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key;
-  return object;
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
 }
